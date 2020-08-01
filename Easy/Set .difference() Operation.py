103f6d16e28d2119ff1b9507aebf745b255e7586
sarthak815 <sarthak815@users.noreply.github.com>
2020-08-02 02:44:21 +0530

    Create Set .difference() Operation.py

---
+++ Easy/Set .difference() Operation.py
@@ -0,0 +1,10 @@
+n1 = int(input())
+nos1 = list(map(int, input().split()))
+nos1 = nos1[:n1]
+nos1 = set(nos1)
+n2 = int(input())
+nos2 = list(map(int, input().split()))
+nos2 = nos2[:n2]
+nos2 = set(nos2)
+nos1 = nos1.difference(nos2)
+print(len(nos1))
